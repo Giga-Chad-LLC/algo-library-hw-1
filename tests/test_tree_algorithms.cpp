3967e74898274a183218439a2504921af3fcc5d5
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2025-01-29 10:08:10 +0100

    feat: create library file structure & cmake configs

---
+++ tests/test_tree_algorithms.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+#include <gtest/gtest.h>
+
+import tree_algorithms;
+
+
+TEST(TreeAlgorithmsTest, PrintMessage) {
+    // Capture output and validate
+    testing::internal::CaptureStdout();
+    trees::printMessage(20);
+    std::string output = testing::internal::GetCapturedStdout();
+
+    EXPECT_EQ(output, "Hello from tree_algorithms module! 20\n");
+}
